99b6dcb6835af816016f664dc515954bbdd92a30
Elias Garzon <eliasgarzon@gmail.com>
2022-01-13 00:47:40 +0200

    corrected cors policy typo

--- app.js
+++ app.js
@@ -66,9 +66,9 @@ const limiter = rateLimit({
 app.use("/api", limiter);
 
 //CORS Policy
-app.use(cors({ credentials: true, origin: "https://localhost:3000" }));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
